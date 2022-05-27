47e3833e4450e624097306b50ca5b4e808277b66
Ciara Gauntt <ci.gauntt@gmail.com>
2022-05-26 21:31:25 -0600

    added css and javascript stuff

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 fetch('https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=arava&ef=STRENGTHS_AND_FORMS')
 .then(response => response.json())
 .then(data => console.log(JSON.stringify(data[1])))
+
+//modal stuff
+var modal = document.getElementById("MedModal");
+var btn = document.getElementById("MedBtn")
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none"
+    }
+}
+
+var btn =document.getElementById("SaveBtn");
+btn.onclick = function() {
+    var medication = document.getElementById('newMed').value;
+    console.log(medication)
+}
