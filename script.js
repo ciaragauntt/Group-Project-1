68dd6708331e3321145533331be6b46d02067a0c
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-05-31 21:02:28 -0600

    Linked JS and started autofill logic

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-fetch('https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=arava&ef=STRENGTHS_AND_FORMS')
-    .then(response => response.json())
-    .then(data => console.log(JSON.stringify(data[1])))
-
-$(function () {
-    $('.calendar-container').calendar();
-})
-
-    // var calendar = $("calendar").calendarGC({
-    //     dayBegin: 0,
-    //     dayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    //     monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Dcember'],
-    //     nextIcon: '&gt;',
-    //     prevIcon: '&lt;',
-    // });
-    .then(response => response.json())
-    .then(data => console.log(JSON.stringify(data[1])))
+var searchInput = document.getElementById('newMed')
+searchInput.onkeydown = function() {
+    var searchData = document.getElementById('newMed').value
+    if(searchData.length >= 3 ) {
+        fetch('https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=' + searchData)
+        .then(response => response.json())
+        .then(data => {console.log(JSON.stringify(data[1]))})
+    }
+}
 
 //modal stuff
-var modal = document.getElementById("MedModal");
+/*var modal = document.getElementById("MedModal");
 var btn = document.getElementById("MedBtn")
 var span = document.getElementsByClassName("close")[0];
 
@@ -40,3 +32,4 @@ btn.onclick = function () {
     var medication = document.getElementById('newMed').value;
     console.log(medication)
 }
+*/
