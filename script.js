6e0f5243f93d1a88f686787c0cf768793c1b013d
Ciara Gauntt <ci.gauntt@gmail.com>
2022-06-05 16:36:14 -0600

    editing javascript to make aside modal work

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ searchInput.onkeyup = function () {
     console.log(searchData)
     if (searchData.length >= 3) {
         $('#newMed').autocomplete({
-            source: function(request, response) {
+            source: function (request, response) {
                 $.getJSON(
                     'https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=' + searchData,
                     request,
-                    function( data, status, xhr ) {
-                      console.log(data)
-                      response( data[1] );
+                    function (data, status, xhr) {
+                        console.log(data)
+                        response(data[1]);
                     }
                 )
             }
-        })       
+        })
     }
 }
 
-    
+
 
 
 const month = new Date();
@@ -45,3 +45,49 @@ window.onclick = function (event) {
     }
 };
 
+const storageInput = document.querySelector('#newMed');
+const text = document.querySelector('.text');
+const savebutton = document.querySelector('#saveBtn');
+const storedInput = localStorage.getItem('MyMedList');
+
+if(storageInput){
+    text.textContent = storedInput
+}
+
+storageInput.addEventListener('input', drug => {
+    text.textContent = drug.target.value
+})
+const saveToLocalStorage = () => {
+    localStorage.setItem('MyMedList', JSON.stringify(input))
+}
+savebutton.addEventListener('click', saveToLocalStorage)
+
+var addedList = document.getElementById('addedList');
+
+console.log(localStorage);
+
+
+//getting medication to save to local storage
+//let medication = [];
+
+//const addMed = (ev) => {
+    //ev.preventDefault(); //stop the form submitting
+    //let med = {
+       // newMed: document.getElementById('newMed').value
+    //}
+    //medication.push(med);
+    //document.forms[0].reset(); //to clear the form for the next entry
+
+    //localStorage.setItem('MyMedList', JSON.stringify(medication));
+    //var list = localStorage.getItem('MyMedList');
+    //list = JSON.parse(list);
+    //$(document).ready(function(){
+    //$("#saveBtn").click(function(){
+        //$("#addedList").append(medication);
+    //});
+//});
+//document.addEventListener('DOMContentLoaded', () => {
+    //document.getElementById('saveBtn').addEventListener('click', addMed);
+//})};
+
+console.log(localStorage);
